31a865f605b8adac84bafb1f4f77152f44602512
jmeade93 <jacob-meade@uiowa.edu>
2015-10-13 22:36:37 +0000

    correctly added flash_message class

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,14 @@ class SessionsController < ApplicationController
     def create
         @user = User.new({:user_id => params['user']['user_id'], :email => params['user']['email']}) 
         if !(User.exists?(user_id: @user.user_id))
-            flash[:notice] = {:class => flash_message, :body => "Invalid user id/email combination"}
+            flash[:notice] = "Invalid user id/email combination"
             redirect_to login_path
         elsif User.where(user_id: @user.user_id).take.email != params['user']['email']
-            flash[:notice] = {:class => flash_message, :body => "Invalid user id/email combination"}
+            flash[:notice] = "Invalid user id/email combination"
             redirect_to login_path
         else
             session[:session_token] = User.where(user_id: @user.user_id).take.session_token
-            flash[:notice] = {:class => flash_message, :body => "You are logged in as #{@user.user_id}"}
+            flash[:notice] = "You are logged in as #{@user.user_id}"
             redirect_to movies_path
         end
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,11 +17,11 @@ class UsersController < ApplicationController
    def create
       @user = User.new(user_params)
       if User.exists?(user_id: @user.user_id)
-         flash[:notice] = {:class => flash_message, :body => "User ID is taken"}     
+         flash[:notice] = "User ID is taken"   
          redirect_to "/users/new"
       else
          @user.create_user!({"user_id" => @user.user_id, "email" => @user.email})
-         flash[:notice] = {:class => flash_message, :body => "Welcome #{@user.user_id}. Your account has been created"}
+         flash[:notice] = "Welcome #{@user.user_id}. Your account has been created"
          redirect_to login_path
       end
    end
